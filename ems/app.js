58118fc1248bae854e386c6ac130c921b2454033
Caren Lierz <carenlierz@hotmail.com>
2018-09-07 12:07:10 -0500

    Adding EMS Project

---
+++ ems/app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+
+var http = require("http");
+
+var path = require("path");
+
+var logger = require("morgan");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(logger("short"));
+
+app.get("/", function(request, response){
+    response.render('index', {
+        message: 'My Application for Handling Employee Records'
+    });
+});
+
+app.get("/New", function(request, response){
+    response.render('new', {
+        message: 'Let\s Add that New Employee!'
+    });
+});
+
+
+
+http.createServer(app).listen(8080, function() {
+
+    console.log("Application started on port 8080!");
+
+});
