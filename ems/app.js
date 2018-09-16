1ddcbf05f7942d8e6d7c25e05845f7a6f175dfb9
Caren Lierz <carenlierz@hotmail.com>
2018-09-16 12:04:46 -0500

    Updating as per week 7 assignment

--- ems/app.js
+++ ems/app.js
@@ -6,6 +6,28 @@ var path = require("path");
 
 var logger = require("morgan");
 
+var mongoose = require("mongoose");
+
+var mongoDB = "mongodb://clierz:husker1ne@ds043388.mlab.com:43388/ems";
+
+mongoose.connect(mongoDB,{
+    useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+db.once("open", function() {
+
+    console.log("Application connected to mLab MongoDB instance");
+
+});
+
+var employee = require("./models/employee");
+
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
@@ -26,7 +48,9 @@ app.get("/New", function(request, response){
     });
 });
 
-
+var employee = new employee({
+    name: "Caren Lierz"
+})
 
 http.createServer(app).listen(8080, function() {
 
